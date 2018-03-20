711fb8710c970c87e78a71e5fe91e8b2ef31dcab
Aleksandr Petrosyan <a-p-petrosyan@yandex.ru>
2018-03-20 17:33:52 +0000

    Initial Commit

---
+++ Ising Model/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Ising Model
+//
+//  Created by Aleksandr Petrosyan on 20/03/2018.
+//  Copyright © 2018 Aleksandr Petrosyan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
