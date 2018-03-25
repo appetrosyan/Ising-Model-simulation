b725178d8ae69c0a758761b9add2d6aef51b28a6
Aleksandr Petrosyan <a-p-petrosyan@yandex.ru>
2018-03-25 18:17:48 +0100

    REDUNDANT

--- Ising Model/simluation.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  simluation.cpp
-//  Ising Model
-//
-//  Created by Aleksandr Petrosyan on 21/03/2018.
-//  Copyright © 2018 Aleksandr Petrosyan. All rights reserved.
-//
-
-#include <stdio.h>
